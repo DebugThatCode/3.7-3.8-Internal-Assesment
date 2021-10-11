00d11d7b3fea2078494fe4812eab41a44e637c8e
Raptor_854 <55820213+Raptor854@users.noreply.github.com>
2021-10-11 15:23:51 +1300

    Final submition

--- js/reservations/v3.js
+++ js/reservations/v3.js
@@ -129,7 +129,7 @@ function validate(element, validationType) { // Validates inputs of pattern, dat
         }
         return true;
     } else {
-        console.error('Invalid validationType' + '\nelement: ' + element + '\nvalidationType: ' + validationType) //  Sends an error message to console if validation type isn't supported
+        console.error('Invalid validationType' + '\nelement: ' + element + '\nvalidationType: ' + validationType); //  Sends an error message to console if validation type isn't supported
     }
 }
 
@@ -170,7 +170,7 @@ function calculateCarCosts() {
     get('mandatory_insurance').innerHTML = '$ ' + reservationDetails.insuranceFee.toFixed(2); // display on form
     get('overlay-mandatory_insurance').innerHTML = '$ ' + reservationDetails.insuranceFee.toFixed(2); // set in overlay}
     // Total
-    reservationDetails['totalPrice'] = reservationDetails.vehicleTotalPrice + reservationDetails.extrasTotalPrice + reservationDetails.insuranceFee + reservationDetails.bookingFee; // calculate total cost
+    reservationDetails.totalPrice = reservationDetails.vehicleTotalPrice + reservationDetails.extrasTotalPrice + reservationDetails.insuranceFee + reservationDetails.bookingFee; // calculate total cost
     get('overlay-totalPrice').innerHTML = '$ ' + reservationDetails.totalPrice.toFixed(2); // set in overlay
 }
 
@@ -205,12 +205,12 @@ const allInputs = {
     'firstName': {
         'tagName': 'id',
         'type': 'pattern',
-        'message': 'Invalid first name (2-30 characters)'
+        'message': 'Invalid first name (2-30 letters)'
     },
     'lastName': {
         'tagName': 'id',
         'type': 'pattern',
-        'message': 'Invalid last name (2-30 characters)'
+        'message': 'Invalid last name (2-30 letters)'
     },
     'DOB': {
         'tagName': 'id',
@@ -236,13 +236,13 @@ const allInputs = {
 
 // Pickup location
 get('pickupLocation').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'select')) { // check if input is valid
+    if (inlineValidate(this, allInputs[this.id].message, 'select')) { // check if input is valid
         let selected = this.selectedIndex;
         let locations = [ // list of locations
             null,
             'Dunedin Airport',
             'Ricky\'s Rides Depo' 
-        ]
+        ];
         reservationDetails.pickupLocation = locations[selected]; // set location
         get('overlay-pickupLocation').innerHTML = locations[selected]; // set in overlay
     } else {
@@ -252,7 +252,7 @@ get('pickupLocation').addEventListener('input', function () {
 // Pickup date
 get('pickupDate').setAttribute('min', todayHTML); // set min pickup date
 get('pickupDate').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'date')) { // check if input is valid
+    if (inlineValidate(this, allInputs[this.id].message, 'date')) { // check if input is valid
         reservationDetails.pickupDate = this.value; // sets pickupDate
         get('overlay-pickupDate').innerHTML = this.value; // sets in overlay
         let pickupJS = getJsDate(this.value); // convert date to js format
@@ -284,7 +284,7 @@ get('returnLocation').addEventListener('input', function () {
 });
 // Return date
 get('returnDate').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'date')) { // check if input is valid
+    if (inlineValidate(this, allInputs[this.id].message, 'date')) { // check if input is valid
         reservationDetails.returnDate = this.value; // set return date
         get('overlay-returnDate').innerHTML = this.value;
         // Calculate car total prices to display
@@ -312,16 +312,18 @@ get('returnDate').addEventListener('input', function () {
 // Car cards
 let carCards = getClass('carCard');
 for (let i = 0; i < carCards.length; i++) {
-    carCards[i].addEventListener('click', function () {
-        carCards[i].setAttribute('data-selected', 'true');
-        if (validate(carCards[i], 'buttonSelect')) {
-            carCards[i].parentElement.setAttribute('data-valid', 'true');
-            reservationDetails.vehicle = carCards[i].getAttribute('data-name');
-            get('overlay-vehicle').innerHTML = reservationDetails.vehicle;
-            reservationDetails.dailyPrice = Number(carCards[i].getAttribute('data-price'));
-            get('overlay-dailyPrice').innerHTML = '$ ' + reservationDetails.dailyPrice.toFixed(2);
+    carCards[i].addEventListener('click', function () { // event listener for selection
+        carCards[i].setAttribute('data-selected', 'true'); // set selected to true
+        if (validate(carCards[i], 'buttonSelect')) { // validate
+            carCards[i].parentElement.setAttribute('data-valid', 'true'); // set valid to true. used by submit to check completed
+            reservationDetails.vehicle = carCards[i].getAttribute('data-name'); // set vehicle
+            get('overlay-vehicle').innerHTML = reservationDetails.vehicle; // set vehicle in overlay
+            reservationDetails.dailyPrice = Number(carCards[i].getAttribute('data-price')); // set daily price
+            get('overlay-dailyPrice').innerHTML = '$ ' + reservationDetails.dailyPrice.toFixed(2); // set dailyprice in overlay
+
             calculateCarCosts();
-            if (reservationDetails.pickupDate == null) {
+
+            if (reservationDetails.pickupDate == null) { // checks if dates have been entered. if not display an error message at dates
                 inlineMessage(get('pickupDate'), true, 'Please enter a date to calculate price');
                 inlineMessage(get('returnDate'), true, 'Please enter a date to calculate price');
             } else if (reservationDetails.returnDate == null) {
@@ -336,19 +338,20 @@ for (let i = 0; i < carCards.length; i++) {
 
 let extras = getClass('extras_select');
 for (let i = 0; i < extras.length; i++) {
-    extras[i].addEventListener('input', function () {
+    extras[i].addEventListener('input', function () { //  event listner for selection
         let selected = [];
         let totalExtraPrice = 0;
         for (let a = 0; a < extras.length; a++) {
-            if (extras[a].checked) {
+            if (extras[a].checked) { // if selected add to list and add to extras price
                 selected.push(extras[a].getAttribute('data-name'));
                 totalExtraPrice += Number(extras[a].getAttribute('data-price'));
             }
         }
-        get('overlay-extras').innerHTML = selected.join('<br>');
-        reservationDetails['extras'] = selected.join(', ');
-        get('overlay-extrasTotalPrice').innerHTML = '$ ' + totalExtraPrice.toFixed(2);
-        reservationDetails['extrasTotalPrice'] = totalExtraPrice;
+        get('overlay-extras').innerHTML = selected.join('<br>'); // set selected axtras in overlay
+        reservationDetails.extras = selected.join(', '); // set extras
+        get('overlay-extrasTotalPrice').innerHTML = '$ ' + totalExtraPrice.toFixed(2); // display extras price in overlay
+        reservationDetails.extrasTotalPrice = totalExtraPrice; // set extras price
+
         calculateCarCosts();
     });
 }
@@ -356,122 +359,123 @@ for (let i = 0; i < extras.length; i++) {
 
 // First name
 get('firstName').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'pattern')) {
-        reservationDetails.firstName = this.value;
-        get('overlay-firstName').innerHTML = this.value;
+    if (inlineValidate(this, allInputs[this.id].message, 'pattern')) { // checks if valid
+        reservationDetails.firstName = this.value; // set value
+        get('overlay-firstName').innerHTML = this.value; // set in overlay
     } else {
-        get('overlay-firstName').innerHTML = '<span class="invalid_entry">' + this.value + '</span>';
+        get('overlay-firstName').innerHTML = '<span class="invalid_entry">' + this.value + '</span>'; // show in overlay as invalid
     }
 });
 // Last Name
 get('lastName').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'pattern')) {
-        reservationDetails.lastName = this.value;
-        get('overlay-lastName').innerHTML = this.value;
+    if (inlineValidate(this, allInputs[this.id].message, 'pattern')) { // checks if valid
+        reservationDetails.lastName = this.value; // set value
+        get('overlay-lastName').innerHTML = this.value; // set in overlay
     } else {
-        get('overlay-lastName').innerHTML = '<span class="invalid_entry">' + this.value + '</span>';
+        get('overlay-lastName').innerHTML = '<span class="invalid_entry">' + this.value + '</span>'; // show in overlay as invalid
     }
 });
 // DOB
 let DOBMin = new Date();
-DOBMin.setTime(todayJS.getTime() - (25 * 365 * 24 * 60 * 60 * 1000));
+DOBMin.setFullYear(todayJS.getFullYear() - (25)); // set min age
 get('DOB').max = getHTMLDate(DOBMin);
 let DOBMax = new Date();
-DOBMax.setTime(todayJS.getTime() - (80 * 365 * 24 * 60 * 60 * 1000));
+DOBMax.setFullYear(todayJS.getFullYear() - (80)); // set max age
 get('DOB').min = getHTMLDate(DOBMax);
 get('DOB').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'date')) {
-        reservationDetails.DOB = this.value;
-        get('overlay-DOB').innerHTML = reservationDetails.DOB;
+    if (inlineValidate(this, allInputs[this.id].message, 'date')) { // checks if valid
+        reservationDetails.DOB = this.value; // set value
+        get('overlay-DOB').innerHTML = reservationDetails.DOB; // set in overlay
     } else {
-        get('overlay-DOB').innerHTML = '<span class="invalid_entry">' + this.value + '</span>';
+        get('overlay-DOB').innerHTML = '<span class="invalid_entry">' + this.value + '</span>'; // show in overlay as invalid
     }
 });
 // Mobile Phone
 get('mobilePhone').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'pattern')) {
-        reservationDetails.mobilePhone = this.value;
-        get('overlay-phone').innerHTML = this.value;
+    if (inlineValidate(this, allInputs[this.id].message, 'pattern')) { // checks if valid
+        reservationDetails.mobilePhone = this.value; // set value
+        get('overlay-phone').innerHTML = this.value; // set in overlay
     } else {
-        get('overlay-phone').innerHTML = '<span class="invalid_entry">' + this.value + '</span>';
+        get('overlay-phone').innerHTML = '<span class="invalid_entry">' + this.value + '</span>'; // show in overlay as invalid
     }
 });
 // Email
 get('email').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'pattern')) {
-        reservationDetails.email = this.value;
-        get('overlay-email').innerHTML = this.value;
+    if (inlineValidate(this, allInputs[this.id].message, 'pattern')) { // checks if valid
+        reservationDetails.email = this.value; // set value
+        get('overlay-email').innerHTML = this.value; // set in overlay
     } else {
-        get('overlay-email').innerHTML = '<span class="invalid_entry">' + this.value + '</span>';
+        get('overlay-email').innerHTML = '<span class="invalid_entry">' + this.value + '</span>'; // show in overlay as invalid
     }
 });
 // Drivers license
 get('driversLicense').addEventListener('input', function () {
-    if (inlineValidate(this, allInputs[this.id]['message'], 'pattern')) {
-        reservationDetails.driversLicense = this.value;
-        get('overlay-driverLicense').innerHTML = this.value;
+    if (inlineValidate(this, allInputs[this.id].message, 'pattern')) { // checks if valid
+        reservationDetails.driversLicense = this.value; // set value
+        get('overlay-driverLicense').innerHTML = this.value; // set in overlay
     } else {
-        get('overlay-driverLicense').innerHTML = '<span class="invalid_entry">' + this.value + '</span>';
+        get('overlay-driverLicense').innerHTML = '<span class="invalid_entry">' + this.value + '</span>'; // show in overlay as invalid
     }
 });
 // Extra comments
 get('comments').addEventListener('input', function () {
-    reservationDetails.comments = this.value;
-})
+    reservationDetails.comments = this.value; // set value
+});
 
 // Declaration checkbox's
-let declaration = {
+let declaration = { // current values
     'termsConditions': false,
     'age': false
 };
 get('_f1-declaration-termsConditions').addEventListener('input', function () {
     if (this.checked) {
-        declaration.termsConditions = true;
+        declaration.termsConditions = true; // set as checked
         if (declaration.age) {
-            get('_f1-submit').removeAttribute('disabled');
+            get('_f1-submit').removeAttribute('disabled'); // enables submit button if both checked
         }
     } else {
-        declaration.termsConditions = false;
-        get('_f1-submit').setAttribute('disabled', '');
+        declaration.termsConditions = false; // set as unchecked
+        get('_f1-submit').setAttribute('disabled', ''); // disables submit button
     }
 });
 get('_f1-declaration-age').addEventListener('input', function () {
     if (this.checked) {
-        declaration.age = true;
+        declaration.age = true; // sets as checked
         if (declaration.termsConditions) {
-            get('_f1-submit').removeAttribute('disabled');
+            get('_f1-submit').removeAttribute('disabled'); // enables submit button if both checked
         }
     } else {
-        declaration.age = false;
-        get('_f1-submit').setAttribute('disabled', '');
+        declaration.age = false; // set as unchecked
+        get('_f1-submit').setAttribute('disabled', ''); // disables submit button
     }
 });
 
 // Submit
 get('_f1-submit').addEventListener('click', function () {
-    this.setAttribute('disabled', '');
+    this.setAttribute('disabled', ''); // disable button to prevent multiple presses
+    // disables declarations
     get('_f1-declaration-termsConditions').setAttribute('disabled', '');
     get('_f1-declaration-age').setAttribute('disabled', '');
 
     // Validate Data
-    let valid = true;
+    let valid = true; // unless set as false by invalid input
     let allInputNames = Object.keys(allInputs);
-    for (let i = 0; i < allInputNames.length; i++) {
-        if (allInputs[allInputNames[i]]['tagName'] == 'id') {
+    for (let i = 0; i < allInputNames.length; i++) { // checks all inputs are valid
+        if (allInputs[allInputNames[i]].tagName == 'id') {
             let element = get(allInputNames[i]);
-            if (!inlineValidate(element, allInputs[allInputNames[i]]['message'], allInputs[allInputNames[i]]['type'])) {
+            if (!inlineValidate(element, allInputs[allInputNames[i]].message, allInputs[allInputNames[i]].type)) { // validates
                 if (valid) { // Calls for first invalid input
-                    pageScrollToElement(element)
+                    pageScrollToElement(element);
                 }
                 valid = false;
             }
-        } else if (allInputs[allInputNames[i]]['tagName'] == 'list') {
+        } else if (allInputs[allInputNames[i]].tagName == 'list') {
             let element = get(allInputNames[i]);
             let elements = element.children;
             for (let a = 0; a < elements.length; a++) {
-                if (!validate(elements[a], allInputs[allInputNames[i]]['message'], allInputs[allInputNames[i]]['type']) && element.dataset.valid != 'true') {
+                if (!validate(elements[a], allInputs[allInputNames[i]].message, allInputs[allInputNames[i]].type) && element.dataset.valid != 'true') { // validates
                     if (valid) { // Calls for first invalid input
-                        pageScrollToElement(element)
+                        pageScrollToElement(element);
                     }
                     valid = false;
                     element.setAttribute('data-valid', 'false');
@@ -482,7 +486,7 @@ get('_f1-submit').addEventListener('click', function () {
 
 
 
-    if (!valid) {
+    if (!valid) { // any invalid, stops function and resets buttons and checkboxs
         this.removeAttribute('disabled');
         get('_f1-declaration-termsConditions').removeAttribute('disabled');
         get('_f1-declaration-age').removeAttribute('disabled');
@@ -491,43 +495,44 @@ get('_f1-submit').addEventListener('click', function () {
 
     // Push data
     reservationRef.push(reservationDetails);
-    console.log('Pushed to database');
+    // console.log('Pushed to database');
 
 
     // Go to postSubmition
     get('_f1').setAttribute('class', 'complete');
     get('_f1-postSubmition-email').innerHTML = reservationDetails.email;
-    countDown(30, get('_f1-refreshCountDown-s'), get('_f1-refreshCountDown'));
+
+    countDown(30, get('_f1-refreshCountDown-s'), get('_f1-refreshCountDown')); // start countdown to reload
 });
 
 function pageScrollToElement(element) {
-    element.scrollIntoView();
+    element.scrollIntoView(); // scrolls to an element
 }
 
 // Post Submition
 
-function countDown(timeRemaining = 1, sSpan, displaySpan) {
-    displaySpan.innerHTML = timeRemaining;
+function countDown(timeRemaining, sSpan, displaySpan) {
+    displaySpan.innerHTML = timeRemaining; // sets time in seconds
     if (timeRemaining == 0) {
-        location.reload();
+        location.reload(); // reloads page
         return;
     } else if (timeRemaining == 1) {
-        sSpan.innerHTML = '';
+        sSpan.innerHTML = ''; // removes s at one second
     }
     setTimeout(function() {
         countDown(timeRemaining - 1, sSpan, displaySpan);
-    }, 1000);
+    }, 1000); // repeats every second
 }
 
 
 // Overlay 
 get('_f1-overlay-close').addEventListener('click', function () {
-    get('_f1-overlay').style.display = 'none';
+    get('_f1-overlay').style.display = 'none'; // hiddes overlay
 });
 
 let openButtons = getClass('overlay-open');
 for (let i = 0; i < openButtons.length; i++) {
     openButtons[i].addEventListener('click', function () {
-        get('_f1-overlay').style.display = 'block';
-    })
+        get('_f1-overlay').style.display = 'block'; // enables overlay
+    });
 }
